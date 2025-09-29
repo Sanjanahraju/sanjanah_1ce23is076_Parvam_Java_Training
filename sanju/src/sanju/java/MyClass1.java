cc785884f51eb1ae0bb5bf902ba9e1984924e33d
Sanjanahraju <sanjanahraju14@gmail.com>
2025-09-29 00:30:17 -0700

    Add files via upload

---
+++ sanju/src/sanju/java/MyClass1.java
@@ -0,0 +1,11 @@
+package sanju.java;
+
+public class MyClass1 {
+	public static void greet() {
+		System.out.println("Hello,java!");
+	}
+	public static void main(String[]args) {
+		greet();
+	}
+
+}
